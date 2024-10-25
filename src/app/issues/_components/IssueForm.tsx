9dba728bf8b9ed5d75ea1b5466f722bc253d8dfe
askari-fahlyani <askari.fahlyani@gmail.com>
2024-10-25 15:38:41 +0200

    Build the edit issue page

---
+++ src/app/issues/_components/IssueForm.tsx
@@ -0,0 +1,110 @@
+"use client";
+import { TextField, Button } from "@radix-ui/themes";
+import dynamic from "next/dynamic";
+import "easymde/dist/easymde.min.css";
+import axios from "axios";
+import { Controller, useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "../../validationSchema";
+import { z } from "zod";
+import { useState } from "react";
+import {ErrorMessage,Spinner} from "@/src/app/Components";
+import { Issue } from "@prisma/client";
+
+
+
+type issueFormData = z.infer<typeof createIssueSchema>;
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+const NewIssuePage = ({issue}:{issue?:Issue}) => {
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+  const [isSubmitted, setSubmitted] = useState<boolean>(false);
+  const [isTitleFill, setIsTitleFill] = useState<boolean>(false);
+  const [isdescriptionFill, setIsDescriptionFill] = useState<boolean>(false);
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<issueFormData>({
+    resolver: zodResolver(createIssueSchema),
+  });
+  const router = useRouter();
+
+  const submitHandler = async (data: issueFormData) => {
+    console.log("form is submitted");
+
+    try {
+      setSubmitted(true);
+      await axios.post("/api/issue", data);
+      router.push("/issues");
+    } catch (error) {
+      setSubmitted(false);
+      if (axios.isAxiosError(error)) {
+        console.log(error);
+      }
+    }
+  };
+
+  const handleClick = () => {
+    setIsClicked(true);
+  };
+  const titleHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsClicked(false);
+    if (e.target.value) {
+      setIsTitleFill(true);
+    } else {
+      setIsTitleFill(false);
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(submitHandler)}
+      className="max-w-[50%] space-y-3  mt-2 "
+    >
+      <TextField.Root
+        defaultValue={issue?.title}
+        {...register("title")}
+        onChange={titleHandler}
+        placeholder="Title"
+      ></TextField.Root>
+      {isClicked && !isTitleFill && (
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+      )}
+      <Controller
+        name="description"
+        control={control}
+        defaultValue={issue?.description}
+        render={({ field: { onChange,value } }) => (
+            <SimpleMDE
+            value={value}
+            placeholder="description"
+            onChange={(newValue: string) => {
+              setIsClicked(false);
+              if (newValue) {
+                setIsDescriptionFill(true);
+              } else {
+                setIsDescriptionFill(false);
+              }
+              onChange(newValue);
+            }}
+          />
+        )}
+      />
+      {isClicked && !isdescriptionFill && (
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+      )}
+
+      <Button disabled={isSubmitted} onClick={handleClick}>
+        Submit New Issue {isSubmitted && <Spinner />}{" "}
+      </Button>
+    </form>
+  );
+};
+
+export default NewIssuePage;
+
--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,105 +1,11 @@
-"use client";
-import { TextField, Button } from "@radix-ui/themes";
-import dynamic from "next/dynamic";
-import "easymde/dist/easymde.min.css";
-import axios from "axios";
-import { Controller, useForm } from "react-hook-form";
-import { useRouter } from "next/navigation";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { createIssueSchema } from "../../validationSchema";
-import { z } from "zod";
-import { useState } from "react";
-import ErrorMessage from "../../Components/ErrorMessage";
-import Spinner from "../../Components/Spinner";
-// import SimpleMDE from 'react-simplemde-editor'
+import React from 'react'
+import IssueForm from '../_components/IssueForm'
 
-type issueForm = z.infer<typeof createIssueSchema>;
-
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-});
 const NewIssuePage = () => {
-  const [isClicked, setIsClicked] = useState<boolean>(false);
-  const [isSubmitted, setSubmitted] = useState<boolean>(false);
-  const [isTitleFill, setIsTitleFill] = useState<boolean>(false);
-  const [isdescriptionFill, setIsDescriptionFill] = useState<boolean>(false);
-  const {
-    register,
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<issueForm>({
-    resolver: zodResolver(createIssueSchema),
-  });
-  const router = useRouter();
-
-  const submitHandler = async (data: issueForm) => {
-    console.log("form is submitted");
-
-    try {
-      setSubmitted(true);
-      await axios.post("/api/issue", data);
-      router.push("/issues");
-    } catch (error) {
-      setSubmitted(false);
-      if (axios.isAxiosError(error)) {
-        console.log(error);
-      }
-    }
-  };
-
-  const handleClick = () => {
-    setIsClicked(true);
-  };
-  const titleHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIsClicked(false);
-    if (e.target.value) {
-      setIsTitleFill(true);
-    } else {
-      setIsTitleFill(false);
-    }
-  };
-
+  
   return (
-    <form
-      onSubmit={handleSubmit(submitHandler)}
-      className="max-w-[50%] space-y-3  mt-2 "
-    >
-      <TextField.Root
-        {...register("title")}
-        onChange={titleHandler}
-        placeholder="Title"
-      ></TextField.Root>
-      {isClicked && !isTitleFill && (
-        <ErrorMessage>{errors.title?.message}</ErrorMessage>
-      )}
-      <Controller
-        name="description"
-        control={control}
-        render={({ field: { onChange } }) => (
-          <SimpleMDE
-            placeholder="description"
-            onChange={(newValue: string) => {
-              setIsClicked(false);
-              if (newValue) {
-                setIsDescriptionFill(true);
-              } else {
-                setIsDescriptionFill(false);
-              }
-              onChange(newValue);
-            }}
-          />
-        )}
-      />
-      {isClicked && !isdescriptionFill && (
-        <ErrorMessage>{errors.description?.message}</ErrorMessage>
-      )}
+   <IssueForm/>
+  )
+}
 
-      <Button disabled={isSubmitted} onClick={handleClick}>
-        Submit New Issue {isSubmitted && <Spinner />}{" "}
-      </Button>
-    </form>
-  );
-};
-
-export default NewIssuePage;
+export default NewIssuePage
