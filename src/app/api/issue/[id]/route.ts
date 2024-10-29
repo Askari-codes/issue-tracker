4f1e59f04995e6d913a19264f0084659d14ee684
askari-fahlyani <askari.fahlyani@gmail.com>
2024-10-29 09:17:56 +0100

    Build an API for deleting issue

--- src/app/api/issue/[id]/route.ts
+++ src/app/api/issue/[id]/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { issueSchema } from "@/src/app/validationSchema";
 import prisma from "@/prisma/client";
+import { error } from "console";
 
 export async function PATCH(
   request: NextRequest,
@@ -22,9 +23,32 @@ if (isDataUnchanged) {
   return NextResponse.json("No changes detected", { status: 204 }); // 204 - No Content
 }
 
+
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
     data: { title: body.title, description: body.description},
   });
   return NextResponse.json(updatedIssue, { status: 201 });
 }
+
+
+
+
+
+export async function DELETE(request:NextResponse,{ params }: { params: { id: string } }) {
+  if(isNaN(parseInt(params.id))){
+    return NextResponse.json({error:'Invalid Id format'},{status:400})
+  }
+ const issue = await prisma.issue.findUnique({
+  where:{id:parseInt(params.id)}
+ })
+ 
+ 
+if(!issue)
+  return NextResponse.json({error:"invalid issue"},{status:404})
+  await prisma.issue.delete({
+    where:{id:issue.id}
+  })
+  return NextResponse.json({ message: 'Issue deleted successfully' }, { status: 200 });
+}
+
