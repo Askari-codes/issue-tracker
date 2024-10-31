a07c0e5f0a7c8b6ad6d1b4bad8b53647b44b1287
askari-fahlyani <askari.fahlyani@gmail.com>
2024-10-31 10:49:51 +0100

    Set up nextAuth

--- src/app/issues/[id]/loading.tsx
+++ src/app/issues/[id]/loading.tsx
@@ -1,28 +1,27 @@
-'use client'
-import { Flex, Card, Box } from '@radix-ui/themes'
-import React from 'react'
-import { Skeleton } from '../../Components'
-import { usePathname,useParams } from 'next/navigation'
+"use client";
+import { Flex, Card, Box } from "@radix-ui/themes";
+import React from "react";
+import { Skeleton } from "../../Components";
+import { usePathname, useParams } from "next/navigation";
 
 const loading = () => {
-  const pathname = usePathname()
-  const {id} = useParams()
-  console.log(pathname);
-  if(pathname!== `/issues/${id}`) return null
+  const pathname = usePathname();
+  const { id } = useParams();
+  if (pathname !== `/issues/${id}`) return null;
   return (
-    <Box className='max-w-xl'>
-       <Skeleton/>
-      <Flex  gap="3" my="3">
-        <Skeleton width='5rem'/>
-        <Skeleton width='8rem'/>
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Flex gap="3" my="3">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
       </Flex>
       <Card>
-      <Skeleton/>
-      <Skeleton/>
-      <Skeleton/>
-        </Card>
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+      </Card>
     </Box>
-  )
-}
+  );
+};
 
-export default loading
+export default loading;
