9ee9e0dc3839aa35fbe06916b37ab27ac88e10fe
askari-fahlyani <askari.fahlyani@gmail.com>
2024-11-06 17:48:37 +0100

    add sucess toast

--- src/app/issues/[id]/AssigneeSelect.tsx
+++ src/app/issues/[id]/AssigneeSelect.tsx
@@ -4,8 +4,8 @@ import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Skeleton from "@/src/app/Components/Skeleton";
-import toast,{Toaster} from 'react-hot-toast'
-
+import toast, { Toaster } from "react-hot-toast";
+import { use } from "react";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
@@ -21,38 +21,50 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   if (isLoading) return <Skeleton />;
   if (error) return null;
 
-  console.log("users", users);
+  
+  const onChangeHandler = (userId: string) => {
+    const targetUser=users?.find(user=>user.id===userId)
+    if (userId) {
+      axios
+        .patch(
+          "/api/issue/" + issue.id,
+          userId === "unassigned"
+            ? { assignedToUserId: null }
+            : { assignedToUserId: userId }
+            
+        )
+        .then(()=>{
+          if(userId==='unassigned'){
+            toast.success(`The issue is unassigned now`)
+          }
+          else{
+
+            toast.success(`The issue is assigned to ${targetUser?.email}`)
+          }
+        })
+        .catch(() => {
+          toast.error("changes could not be saved");
+        });
+    }
+  };
 
   return (
     <>
-    <Select.Root
-      onValueChange={(userId) => {
-        if (userId) {
-          axios.patch(
-            "/xapi/issue/" + issue.id,
-            userId === "unassigned"
-              ? { assignedToUserId: null }
-              : { assignedToUserId: userId }
-          ).catch(()=>{
-            toast.error('changes could not be saved')
-          });
-        }
-      }}
-    >
-      <Select.Trigger placeholder="Asignee..." />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>suggestions...</Select.Label>
-          <Select.Item value="unassigned">Unassigned</Select.Item>
-          {users?.map((user) => (
-            <Select.Item value={user.id} key={user.id}>
-              {user.email}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
-    <Toaster/>
+      <Select.Root onValueChange={onChangeHandler}>
+        <Select.Trigger placeholder="Asignee..." />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>suggestions...</Select.Label>
+            <Select.Item value="unassigned">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item value={user.id} key={user.id}>
+                {user.email}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
     </>
   );
 };
