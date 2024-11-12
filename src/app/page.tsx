ff2052f2831a833580c464fe3681eb3474904f87
askari-fahlyani <askari.fahlyani@gmail.com>
2024-11-12 16:43:36 +0100

    Implement pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-import Image from 'next/image'
+'use client'
+import { useEffect } from 'react';
 import Pagination from './Components/Pagination'
+import { log } from 'console';
 
-export default function Home() {
+
+export default function Home({searchParams}:{searchParams:{page:string}}) {
+ useEffect(() => {
+  console.log(searchParams);
+  
+ }, []);
   return (
   <div className='text-black'>
 dashboard
-<Pagination itemCount={100} pageSize={10} currentPage={10}/>
+<Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)}/>
   </div>
   )
 
